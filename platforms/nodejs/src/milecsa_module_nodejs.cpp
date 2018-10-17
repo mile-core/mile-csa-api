897e82d1cfe5be93f6ba689c104f0a7310c3fca8
lotus-mile <develop@mile.global>
2018-10-17 21:48:46 +0300

    code cleaned up

--- platforms/nodejs/src/milecsa_module_nodejs.cpp
+++ platforms/nodejs/src/milecsa_module_nodejs.cpp
@@ -2,6 +2,8 @@
 // Created by lotus mile on 17/10/2018.
 //
 
+#include <functional>
+#include <optional>
 #include "milecsa_module_nodejs.hpp"
 
 namespace milecsa::nodejs {
@@ -29,22 +31,25 @@ namespace milecsa::nodejs {
             info.GetReturnValue().Set(info.Holder());
     }
 
+    static inline void __update_key(const std::optional<milecsa::keys::Pair> &pair, const Nan::FunctionCallbackInfo<v8::Value>& info){
+        v8::Local<v8::Object> object = Nan::New<v8::Object>();
+
+        v8::Local<v8::String> pk_name = Nan::New("public_key").ToLocalChecked();
+        v8::Local<v8::Value> pk_value = Nan::New(pair->get_public_key().encode()).ToLocalChecked();
+
+        v8::Local<v8::String> pvk_name = Nan::New("private_key").ToLocalChecked();
+        v8::Local<v8::Value> pvk_value = Nan::New(pair->get_private_key().encode()).ToLocalChecked();
+
+        Nan::Set(object, pk_name, pk_value);
+        Nan::Set(object, pvk_name, pvk_value);
+
+        info.GetReturnValue().Set(object);
+    }
+
     void Random(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 
         if (auto pair = milecsa::keys::Pair::Random(error_handler)){
-
-            v8::Local<v8::Object> object = Nan::New<v8::Object>();
-
-            v8::Local<v8::String> pk_name = Nan::New("public_key").ToLocalChecked();
-            v8::Local<v8::Value> pk_value = Nan::New(pair->get_public_key().encode()).ToLocalChecked();
-
-            v8::Local<v8::String> pvk_name = Nan::New("private_key").ToLocalChecked();
-            v8::Local<v8::Value> pvk_value = Nan::New(pair->get_private_key().encode()).ToLocalChecked();
-
-            Nan::Set(object, pk_name, pk_value);
-            Nan::Set(object, pvk_name, pvk_value);
-
-            info.GetReturnValue().Set(object);
+            __update_key(pair,info);
         }
     }
 
@@ -62,52 +67,76 @@ namespace milecsa::nodejs {
         v8::String::Utf8Value phrase(str);
 
         if (auto pair = milecsa::keys::Pair::WithSecret(*phrase, error_handler)){
-
-            v8::Local<v8::Object> object = Nan::New<v8::Object>();
-
-            v8::Local<v8::String> pk_name = Nan::New("public_key").ToLocalChecked();
-            v8::Local<v8::Value> pk_value = Nan::New(pair->get_public_key().encode()).ToLocalChecked();
-
-            v8::Local<v8::String> pvk_name = Nan::New("private_key").ToLocalChecked();
-            v8::Local<v8::Value> pvk_value = Nan::New(pair->get_private_key().encode()).ToLocalChecked();
-
-            Nan::Set(object, pk_name, pk_value);
-            Nan::Set(object, pvk_name, pvk_value);
-
-            info.GetReturnValue().Set(object);
+            __update_key(pair,info);
         }
+    }
+
+    static inline bool __check_one_args(const Nan::FunctionCallbackInfo<v8::Value>& info, const std::string &name, const std::string &what){
+        std::string prefix = "milecsa error: " + name + " - expected ";
+        if(info.Length() != 1) {
+            prefix += what;
+            Nan::ThrowError(Nan::New(prefix).ToLocalChecked());
+            return false;
+        }
+
+        if(!info[0]->IsString()) {
+            prefix += "argument string";
+            Nan::ThrowError(Nan::New(prefix).ToLocalChecked());
+            return false;
+        }
+
+        return true;
     }
 
     void FromPrivateKey(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 
-        if(info.Length() != 1) {
-            return Nan::ThrowError(Nan::New("milecsa error: FromPrivateKey - expected private key").ToLocalChecked());
-        }
-
-        if(!info[0]->IsString()) {
-            return Nan::ThrowError(Nan::New("Vmilecsa error: FromPrivateKey - expected argument string").ToLocalChecked());
-        }
+        if(!__check_one_args(info,"FromPrivateKey","private key"))
+            return;
 
         v8::Local<v8::String> str = v8::Local<v8::String>::Cast(info[0]);
         v8::String::Utf8Value pvk(str);
 
         if (auto pair = milecsa::keys::Pair::FromPrivateKey(*pvk, error_handler)){
-
-            v8::Local<v8::Object> object = Nan::New<v8::Object>();
-
-            v8::Local<v8::String> pk_name = Nan::New("public_key").ToLocalChecked();
-            v8::Local<v8::Value> pk_value = Nan::New(pair->get_public_key().encode()).ToLocalChecked();
-
-            v8::Local<v8::String> pvk_name = Nan::New("private_key").ToLocalChecked();
-            v8::Local<v8::Value> pvk_value = Nan::New(pair->get_private_key().encode()).ToLocalChecked();
-
-            Nan::Set(object, pk_name, pk_value);
-            Nan::Set(object, pvk_name, pvk_value);
-
-            info.GetReturnValue().Set(object);
+            __update_key(pair,info);
         }
     }
 
+    void ValidatePublicKey(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+
+        if(!__check_one_args(info,"ValidatePublicKey","public key"))
+            return;
+
+        v8::Local<v8::String> str = v8::Local<v8::String>::Cast(info[0]);
+        v8::String::Utf8Value pk(str);
+
+        if (milecsa::keys::Pair::ValidatePublicKey(*pk, error_handler)){
+            info.GetReturnValue().Set(Nan::True());
+        } else{
+            info.GetReturnValue().Set(Nan::False());
+        }
+    }
+
+    void ValidatePrivateKey(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+        
+        if(!__check_one_args(info,"ValidatePrivateKey","private key"))
+            return;
+
+        v8::Local<v8::String> str = v8::Local<v8::String>::Cast(info[0]);
+        v8::String::Utf8Value pvk(str);
+
+        if (milecsa::keys::Pair::ValidatePrivateKey(*pvk, error_handler)){
+            info.GetReturnValue().Set(Nan::True());
+        } else{
+            info.GetReturnValue().Set(Nan::False());
+        }
+    }
+
+    static inline void __set_function(v8::Local<v8::Object> &object, const std::string name, const v8::Local<v8::FunctionTemplate> &tpl){
+        v8::Local<v8::String> key = Nan::New(name).ToLocalChecked();
+        v8::Local<v8::Function> fn = tpl->GetFunction();
+        fn->SetName(Nan::New(name.c_str()).ToLocalChecked());
+        Nan::Set(object, key, fn);
+    }
 
     NAN_GETTER(Module::HandleGetters) {
             Module* self = Nan::ObjectWrap::Unwrap<Module>(info.This());
@@ -117,24 +146,11 @@ namespace milecsa::nodejs {
 
                 v8::Local<v8::Object> object = Nan::New<v8::Object>();
 
-                v8::Local<v8::String> rnd = Nan::New("Random").ToLocalChecked();
-                v8::Local<v8::FunctionTemplate> tpl_rnd = Nan::New<v8::FunctionTemplate>(Random);
-                v8::Local<v8::Function> fn_rnd = tpl_rnd->GetFunction();
-                fn_rnd->SetName(Nan::New("Random").ToLocalChecked());
-
-                v8::Local<v8::String> secret = Nan::New("WithSecret").ToLocalChecked();
-                v8::Local<v8::FunctionTemplate> tpl_secret = Nan::New<v8::FunctionTemplate>(WithSecret);
-                v8::Local<v8::Function> fn_secret = tpl_secret->GetFunction();
-                fn_secret->SetName(Nan::New("WithSecret").ToLocalChecked());
-
-                v8::Local<v8::String> private_k = Nan::New("FromPrivateKey").ToLocalChecked();
-                v8::Local<v8::FunctionTemplate> tpl_private_k = Nan::New<v8::FunctionTemplate>(FromPrivateKey);
-                v8::Local<v8::Function> fn_private_k = tpl_private_k->GetFunction();
-                fn_private_k->SetName(Nan::New("FromPrivateKey").ToLocalChecked());
-
-                Nan::Set(object, rnd, fn_rnd);
-                Nan::Set(object, secret, fn_secret);
-                Nan::Set(object, private_k, fn_private_k);
+                __set_function(object, "Random", Nan::New<v8::FunctionTemplate>(Random));
+                __set_function(object, "WithSecret", Nan::New<v8::FunctionTemplate>(WithSecret));
+                __set_function(object, "FromPrivateKey", Nan::New<v8::FunctionTemplate>(FromPrivateKey));
+                __set_function(object, "ValidatePublicKey", Nan::New<v8::FunctionTemplate>(ValidatePublicKey));
+                __set_function(object, "ValidatePrivateKey", Nan::New<v8::FunctionTemplate>(ValidatePrivateKey));
 
                 info.GetReturnValue().Set(object);
 
--- platforms/nodejs/tests/testAddon.js
+++ platforms/nodejs/tests/testAddon.js
@@ -23,12 +23,26 @@ describe("Boost Based Addon", function() {
         console.log("From Private key private key: " + from_private.private_key)
         console.log("From Private key public key : " + from_private.public_key)
 
+        var ret = Module.Pair.ValidatePublicKey(from_private.public_key)
+        console.log("... ValidatePublicKey : " + ret)
+
+        ret = Module.Pair.ValidatePrivateKey(from_private.private_key)
+        console.log("... ValidatePrivateKey : " + ret)
+
+        try {
+            Module.Pair.ValidatePublicKey("")
+        }
+        catch (e) {
+            console.log("Invalide ValidatePublicKey key: " + e)
+        }
+
         try {
             var from_private = Module.Pair.FromPrivateKey("invalide")
         }
         catch (e) {
             console.log("Invalide Private key: " + e)
         }
+
 
         assert.equal(from_private.public_key, with_secret.public_key);
     });
